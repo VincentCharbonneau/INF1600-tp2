9165840e7dd118854da6143b8f64f09456ac4a1e
VincentCharbonneau <vicha2a>
2024-02-27 12:41:29 -0500

    fini monnaie

--- monnaie/main.s
+++ monnaie/main.s
@@ -1,21 +1,72 @@
 .data
 caisse:
-.int 25, 10, 5, 1 
-#TODO initialiser les strings pour l'affichage des billets
+.int 25, 10, 5, 1
 
+formated_string25: .asciz "Le nombre de billets de 25$ est: %d\n"
+formated_string10: .asciz "Le nombre de billets de 10$ est: %d\n"
+formated_string5: .asciz "Le nombre de billets de 5$ est: %d\n"
+formated_string1: .asciz "Le nombre de billets de 1$ est: %d\n"
+
+amount: 
+.int 1000
 
 .text
 .globl main
-main : 
+main :   
 push %ebp
 mov %esp,%ebp
 push %ebx
-#TODO: Répondre a la question
+
+movl amount, %eax
+movl $0, %ebx
+
+movl caisse(,%ebx,4), %ecx
+movl $0, %edx
+divl %ecx
+pushl %edx
+pushl %eax
+pushl $formated_string25
+call printf
+addl $8, %esp
+popl %eax
+
+addl $1, %ebx
+
+movl caisse(,%ebx,4), %ecx
+movl $0, %edx
+divl %ecx
+pushl %edx
+pushl %eax
+pushl $formated_string10
+call printf
+addl $8, %esp
+popl %eax
+
+addl $1, %ebx
+
+movl caisse(,%ebx,4), %ecx
+movl $0, %edx
+divl %ecx
+pushl %edx
+pushl %eax
+pushl $formated_string5
+call printf
+addl $8, %esp
+popl %eax
+
+addl $1, %ebx
+
+movl caisse(,%ebx,4), %ecx
+movl $0, %edx
+divl %ecx
+pushl %edx
+pushl %eax
+pushl $formated_string1
+call printf
+addl $8, %esp
+popl %eax
 
 end:
 popl %ebx
 popl %ebp
 ret
-
-
-
