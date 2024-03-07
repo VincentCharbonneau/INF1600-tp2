b6be88d8bcfe808b83f1424a90d794524784b7c4
celeblond <celeblond12@gmail.com>
2024-03-07 18:11:39 -0500

    Euler entière

--- e_euler_approx/e_euler_entiere/e_euler_entiere.s
+++ e_euler_approx/e_euler_entiere/e_euler_entiere.s
@@ -8,8 +8,21 @@ push %ebp
 mov %esp,%ebp
 push %ebx
 
-##VOTRE CODE ICI
+movl $1, %eax         # Initialiser le numérateur à 1
+movl $1, %ebx         # Initialiser le dénominateur à 1
+movl $1, %ecx         # Initialiser le résultat à 1
+movl $1, %edi         # Initialiser le compteur à 1
+movl $10, %edx        # Nombre d'itérations
 
+approximation_e:
+    mul %edi          # Multiplier le compteur par le résultat
+    div %ebx          # Diviser le résultat par le dénominateur
+    incl %edi          # Augmenter le compteur
+    mul %edi          # Multiplier le dénominateur par le compteur
+    cmp $10, %edi      # Vérifier si nous avons atteint 10 itérations
+    je end             # Si oui, sortir de la boucle
+    loop approximation_e
+    
 pop %ebx
 pop %ebp
 ret
