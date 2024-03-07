ebbf342074d8cc68aef4709f5f523170afd2608d
VincentCharbonneau <vicha2a>
2024-03-07 17:18:44 -0500

    GDB mieux fait, DONE

--- chaine_de_caractere/GDB/main.s
+++ chaine_de_caractere/GDB/main.s
@@ -7,43 +7,43 @@ den:
 .asciz "Hi\n"
 
 
+
 .text
 .globl main
 
-
-main : 
-push %ebp
-mov %esp,%ebp
-push %ebx
+main: 
+pushl %ebp
+movl %esp,%ebp
+pushl %ebx
 
 mask:
-xor %eax, %eax          
+xor %eax, %eax
 xor %ecx, %ecx
 xor %ebx, %ebx
 
 setup:
-movl $strings, %esi     
+movl $strings, %esi
 
 n_c:
-lodsb                  
+lodsb
 
-d:              
+d:
 cmp $0x20, %al
 je n_c2
 cmp $0x41, %al
 jb n_c2
-cmp $0x5A, %al
-jb a
-cmp $0x61, %al
+cmp $0x5A, %al 
+jb a            
+cmp $0x61, %al 
 jb n_c2
-cmp $0x7A, %al 
+cmp $0x7A, %al
 jbe a
 
-a:       
+a:
 addl %eax, %ecx
 jmp n_c
 
-n_c2:
+n_c2: 
 lodsb
 
 d2:
@@ -52,7 +52,7 @@ jz r
 cmp $0x41, %al
 jb n_c2
 cmp $0x5A, %al
-jb a_2
+jb a_2         
 cmp $0x61, %al
 jb n_c2
 cmp $0x7A, %al 
@@ -64,23 +64,23 @@ jmp n_c2
 
 r:
 movl %ecx, %eax
-
-movl $0, %edx
-divl %ebx
-cmp $0x00, %eax
-ja e_d              
+movl $0, %edx     #ERREUR1
+divl %ebx 
+cmp $0x00, %eax 
+ja e_d            #ERREUR2 
 jmp e_n
-             
+
 
 e_n:
-push $num
+pushl $num
 call printf
- 
+addl $4, %esp   #ERREUR 3
 jmp bye
 
 e_d:
-push $den
+pushl $den
 call printf
+addl $4, %esp   #ERREUR 4
 
 
 bye:
