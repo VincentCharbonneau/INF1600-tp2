64f9aa62e576987250d5f1a293d0877abe875c8b
celeblond <celeblond12@gmail.com>
2024-03-07 18:19:52 -0500

    Update euler entiere

--- e_euler_approx/e_euler_entiere/e_euler_entiere.s
+++ e_euler_approx/e_euler_entiere/e_euler_entiere.s
@@ -8,21 +8,28 @@ push %ebp
 mov %esp,%ebp
 push %ebx
 
-movl $1, %eax         # Initialiser le numérateur à 1
-movl $1, %ebx         # Initialiser le dénominateur à 1
-movl $1, %ecx         # Initialiser le résultat à 1
-movl $1, %edi         # Initialiser le compteur à 1
-movl $10, %edx        # Nombre d'itérations
+movl $1, %eax
+movl $1, %ebx
+movl $1, %ecx
+movl $1, %edi
+movl $10, %edx
 
 approximation_e:
-    mul %edi          # Multiplier le compteur par le résultat
-    div %ebx          # Diviser le résultat par le dénominateur
-    incl %edi          # Augmenter le compteur
-    mul %edi          # Multiplier le dénominateur par le compteur
-    cmp $10, %edi      # Vérifier si nous avons atteint 10 itérations
-    je end             # Si oui, sortir de la boucle
+    mul %edi
+    div %ebx
+    incl %edi
+    mul %edi
+    cmp $10, %edi
+    je end
     loop approximation_e
-    
+
 pop %ebx
 pop %ebp
 ret
+
+#Q3.2.2
+#On remarque qu'après 2 itérations, on a ajouté 1 deux fois '1',
+#mais ensuite, on ajoute des valeurs inférieurs à '1', et puisqu'on
+#travaille avec des nombre entiers, on ajoute en fait '0'. On remarque
+#donc qu'après 2 itérations, en faire davantage ne change pas le résultat
+#final, qui est '2'.
