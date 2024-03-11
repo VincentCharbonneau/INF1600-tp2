25bea05e177f92de2707d702132500bcb57c2e87
celeblond <celeblond12@gmail.com>
2024-03-10 21:16:52 -0400

    Euler fonctionne

--- e_euler_approx/e_euler_entiere/e_euler_entiere.s
+++ e_euler_approx/e_euler_entiere/e_euler_entiere.s
@@ -2,15 +2,15 @@
 .globl e_euler_entiere
 
 e_euler_entiere:
-push %ebp
-mov %esp, %ebp
-push %ebx
-mov 8(%ebp), %ecx  #nombre iterations
+    push %ebp
+    mov %esp, %ebp
+    push %ebx
+    mov 8(%ebp), %ecx  #nombre iterations
 
-mov $1, %eax       #numerateur
-mov $1, %ebx       #denominateur
-mov $0, %edx       #somme
-mov $0, %edi       #compteur
+    mov $1, %eax       #numerateur
+    mov $1, %ebx       #denominateur
+    mov $0, %edx       #somme
+    mov $0, %edi       #compteur
 
 iteration0:        #pour 1/0!
     cmp $0, %ecx
@@ -39,6 +39,7 @@ end:
     pop %ebx
     pop %ebp
     ret
+
 
 #Q3.2.2
 #On remarque qu'après 2 itérations, on a ajouté 1 deux fois '1',
--- e_euler_approx/e_euler_flottante/e_euler_flottante.s
+++ e_euler_approx/e_euler_flottante/e_euler_flottante.s
@@ -1,10 +1,8 @@
 .data
 iteration:
 .int 3
-iteration_FPU:  
-.float 3      
 factorial_value:
-.float 0
+.float 1
 factorial:
 .float 0
 e:
@@ -17,13 +15,53 @@ one:
 .globl e_euler_flottante
 
 e_euler_flottante:
+    push %ebp               
+    mov %esp,%ebp
+    push %ebx
 
-push %ebp               
-mov %esp,%ebp
-push %ebx
+start :
+    #verifier si iteration est 0
+    mov iteration, %ecx
+    cmp $0, %ecx
+    jz end
 
-## VOTRE CODE ICI 
+    #ajouter 1 pour 1/0!
+    flds e
+    flds one
+    faddp
+    fstps e
 
-pop %ebx
-pop %ebp
-ret
+    #decrementer iteration et verifier si 0
+    dec %ecx
+    jz end
+
+approximation_e:
+    # incrémenter le compteur de 1
+    flds factorial
+    flds one
+    faddp
+    fstps factorial
+
+    # multiplier le denominateur et le compteur pour calculer factorielle
+    flds factorial
+    flds factorial_value
+    fmulp
+    fstps factorial_value
+
+    # divison
+    flds factorial_value
+    flds one
+    fdivp
+
+    # ajouter le résultat à la somme
+    flds e
+    faddp
+    fstps e
+
+    loop approximation_e
+    
+end:
+    flds e
+    pop %ebx
+    pop %ebp
+    ret
